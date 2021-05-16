eb255dbc5701f9a0ce7e7c1a5e42b93f0bf7f781
Manuel Stoeckl <code@mstoeckl.com>
2021-05-16 13:21:03 -0400

    React only to most recent configure event
    When outputs are dynamically resized, as can happen when sway
    is run nested with its wayland or x11 backend, layer shell programs
    receive a stream of configure events. In such cases, only rendering
    a frame for the last configure event avoids wasted computation.

--- main.c
+++ main.c
@@ -70,6 +70,9 @@ struct swaybg_output {
 	uint32_t width, height;
 	int32_t scale;
 
+	uint32_t configure_serial;
+	bool dirty;
+
 	struct wl_list link;
 };
 
@@ -157,8 +160,8 @@ static void layer_surface_configure(void *data,
 	struct swaybg_output *output = data;
 	output->width = width;
 	output->height = height;
-	zwlr_layer_surface_v1_ack_configure(surface, serial);
-	render_frame(output);
+	output->configure_serial = serial;
+	output->dirty = true;
 }
 
 static void layer_surface_closed(void *data,
@@ -530,7 +533,15 @@ int main(int argc, char **argv) {
 
 	state.run_display = true;
 	while (wl_display_dispatch(state.display) != -1 && state.run_display) {
-		// This space intentionally left blank
+		wl_list_for_each(output, &state.outputs, link) {
+			if (output->dirty) {
+				output->dirty = false;
+				zwlr_layer_surface_v1_ack_configure(
+						output->layer_surface,
+						output->configure_serial);
+				render_frame(output);
+			}
+		}
 	}
 
 	struct swaybg_output *tmp_output;
